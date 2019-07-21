6c275ae252466af19eb7e240cf87ad15fd71a4cc
Archangel354 <rduncan354@gmail.com>
2019-07-21 14:59:39 -0400

    I added internet permission to AndroidManifest.xml This app uses a video fragment, Exoplayer, and butterknife The fragment only has 2 methods:  CreateView and OnDestroyView - the number of methods in the fragment may increase later I put the return statement at the bottom of the onCreateView method I also added unbinder Butterknife.bind(this, rootView) to the OnCreateView method Finally, I added a new instance of the fragment in the MainActivity

--- app/src/main/java/com/example/fragmentwithexoplayer/ExoPlayerFragment.java
+++ app/src/main/java/com/example/fragmentwithexoplayer/ExoPlayerFragment.java
@@ -1,108 +1,97 @@
 package com.example.fragmentwithexoplayer;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link ExoPlayerFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ExoPlayerFragment#newInstance} factory method to
+
  * create an instance of this fragment.
  */
 public class ExoPlayerFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    @BindView(R.id.imgInstructionVideo)
+    PlayerView mPlayerView;
+    private SimpleExoPlayer mPlayer;
+    private Unbinder unbinder;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
+    // URL of the video to stream
+    public static String urlString = "https://d17h27t6h515a5.cloudfront.net/topher/2017/April/58ffd974_-intro-creampie/-intro-creampie.mp4";
 
     public ExoPlayerFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ExoPlayerFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ExoPlayerFragment newInstance(String param1, String param2) {
-        ExoPlayerFragment fragment = new ExoPlayerFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_exo_player, container, false);
-    }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+        String urlString = "https://d17h27t6h515a5.cloudfront.net/topher/2017/April/58ffd974_-intro-creampie/-intro-creampie.mp4";
+
+        View rootView = inflater.inflate(R.layout.fragment_exo_player, container, false);
+        unbinder = ButterKnife.bind(this, rootView);
+                //Uri myUri = Uri.parse(urlString);
+
+        Uri anyUri = Uri.parse(urlString);
+        Log.i("StepDetailActivity", "urlString: " + urlString + ".");
+        if (urlString.isEmpty()) {
+
+            mPlayerView.setVisibility(View.GONE);
+            //imgNoVideo.setVisibility(View.VISIBLE);
         }
+        else {
+            // Initialize the player view
+            //mPlayerView = getView().findViewById(R.id.imgInstructionVideo);
+            mPlayerView.setVisibility(View.VISIBLE);
+            //imgNoVideo.setVisibility(View.GONE);
+            mPlayer = ExoPlayerFactory.newSimpleInstance(getContext(), new DefaultTrackSelector());
+            mPlayerView.setPlayer(mPlayer);
+            DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(
+                    getContext(),
+                    Util.getUserAgent(getContext(), getString(R.string.app_name)));
+            ExtractorMediaSource mediaSource = new ExtractorMediaSource.Factory(dataSourceFactory)
+                    .createMediaSource(anyUri);
+
+            mPlayer.prepare(mediaSource);
+            mPlayer.setPlayWhenReady(true);
+        }
+        return rootView;
+
     }
 
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        // Release the player when it is not needed
+        mPlayer.release();
     }
 }
--- app/src/main/java/com/example/fragmentwithexoplayer/MainActivity.java
+++ app/src/main/java/com/example/fragmentwithexoplayer/MainActivity.java
@@ -1,13 +1,49 @@
 package com.example.fragmentwithexoplayer;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnExoplayer;
+
+    @BindView(R.id.btnExoPlayer) Button BtnExoplayer;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btnExoPlayer)
+
+    public void sayHi(Button btnExoplayer){
+        btnExoplayer.setText("Hello!");
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        //Log.i("StepDetailActivity", "mDescription: " + mDescription + ".");
+
+        //String step = getIntent().getStringExtra(EXTRA_STEP);
+        //Bundle arguments = new Bundle();
+        //arguments.putString(EXTRA_STEP, sDescription);
+        //arguments.putString(EXTRA_VIDEO, sUrlString);
+        ExoPlayerFragment exoPlayerFragment = new ExoPlayerFragment();
+        //descriptionFragment.setArguments(arguments);
+        fragmentManager.beginTransaction()
+                .add(R.id.video_container, exoPlayerFragment)
+                .commit();
+    }
+    {
+
     }
 }
